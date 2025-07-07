0d89359d8fdd3cfab95377973ffcd4ba078a5324
Tezahc <tezahc23@gmail.com>
2025-07-07 17:09:22 +0200

    reindent file to 4 spaces

--- scrip_totu.py
+++ scrip_totu.py
@@ -33,7 +33,6 @@ print(f"video_width: {video_width}")
 print(f"video_height: {video_height}")
 
 # Initialize tracker, classifier and counter.
-
 # Folder with pose class CSVs. That should be the same folder you used while
 # building classifier to output CSVs.
 pose_samples_folder = 'fitness_poses_csvs_out'
@@ -47,10 +46,10 @@ pose_embedder = FullBodyPoseEmbedding()
 # Initialize classifier.
 # Check that you are using the same parameters as during bootstrapping.
 pose_classifier = PoseClassifier(
-    pose_samples_folder=pose_samples_folder,
-    pose_embedder=pose_embedder,
-    top_n_by_max_distance=30,
-    top_n_by_mean_distance=10)
+        pose_samples_folder=pose_samples_folder,
+        pose_embedder=pose_embedder,
+        top_n_by_max_distance=30,
+        top_n_by_mean_distance=10)
 
 # # Uncomment to validate target poses used by classifier and find outliers.
 # outliers = pose_classifier.find_pose_sample_outliers()
@@ -58,94 +57,94 @@ pose_classifier = PoseClassifier(
 
 # Initialize EMA smoothing.
 pose_classification_filter = EMADictSmoothing(
-    window_size=10,
-    alpha=0.2)
+        window_size=10,
+        alpha=0.2)
 
 # Initialize counter.
 repetition_counter = RepetitionCounter(
-    class_name=class_name,
-    enter_threshold=6,
-    exit_threshold=4)
+        class_name=class_name,
+        enter_threshold=6,
+        exit_threshold=4)
 
 # Initialize renderer.
 pose_classification_visualizer = PoseClassificationVisualizer(
-    class_name=class_name,
-    plot_x_max=video_n_frames,
-    # Graphic looks nicer if it's the same as `top_n_by_mean_distance`.
-    plot_y_max=10)
+        class_name=class_name,
+        plot_x_max=video_n_frames,
+        # Graphic looks nicer if it's the same as `top_n_by_mean_distance`.
+        plot_y_max=10)
 
 
 # Open output video.
-out_video = cv2.VideoWriter(out_video_path, cv2.VideoWriter_fourcc(*'mp4v'), video_fps, (video_width, video_height))
+out_video = cv2.VideoWriter(out_video_path, cv2.VideoWriter_four(*'mp4v'), video_fps, (video_width, video_height))
 
 frame_idx = 0
 output_frame = None
 with tqdm.tqdm(total=video_n_frames, position=0, leave=True) as pbar:
-  while True:
-    # Get next frame of the video.
-    success, input_frame = video_cap.read()
-    if not success:
-      print("unable to read input video frame, breaking!")
-      break
+    while True:
+        # Get next frame of the video.
+        success, input_frame = video_cap.read()
+        if not success:
+            print("unable to read input video frame, breaking!")
+            break
 
-    # Run pose tracker.
-    input_frame = cv2.cvtColor(input_frame, cv2.COLOR_BGR2RGB)
-    result = pose_tracker.process(image=input_frame)
-    pose_landmarks = result.pose_landmarks
+        # Run pose tracker.
+        input_frame = cv2.cvtColor(input_frame, cv2.COLOR_BGR2RGB)
+        result = pose_tracker.process(image=input_frame)
+        pose_landmarks = result.pose_landmarks
 
-    # Draw pose prediction.
-    output_frame = input_frame.copy()
-    if pose_landmarks is not None:
-      mp_drawing.draw_landmarks(
-          image=output_frame,
-          landmark_list=pose_landmarks,
-          connections=mp_pose.POSE_CONNECTIONS)
-    
-    if pose_landmarks is not None:
-      # Get landmarks.
-      frame_height, frame_width = output_frame.shape[0], output_frame.shape[1]
-      pose_landmarks = np.array([[lmk.x * frame_width, lmk.y * frame_height, lmk.z * frame_width]
-                                 for lmk in pose_landmarks.landmark], dtype=np.float32)
-      assert pose_landmarks.shape == (33, 3), 'Unexpected landmarks shape: {}'.format(pose_landmarks.shape)
+        # Draw pose prediction.
+        output_frame = input_frame.copy()
+        if pose_landmarks is not None:
+            mp_drawing.draw_landmarks(
+                    image=output_frame,
+                    landmark_list=pose_landmarks,
+                    connections=mp_pose.POSE_CONNECTIONS)
+        
+        if pose_landmarks is not None:
+            # Get landmarks.
+            frame_height, frame_width = output_frame.shape[0], output_frame.shape[1]
+            pose_landmarks = np.array([[lmk.x * frame_width, lmk.y * frame_height, lmk.z * frame_width]
+                                                                 for lmk in pose_landmarks.landmark], dtype=np.float32)
+            assert pose_landmarks.shape == (33, 3), 'Unexpected landmarks shape: {}'.format(pose_landmarks.shape)
 
-      # Classify the pose on the current frame.
-      pose_classification = pose_classifier(pose_landmarks)
+            # Classify the pose on the current frame.
+            pose_classification = pose_classifier(pose_landmarks)
 
-      # Smooth classification using EMA.
-      pose_classification_filtered = pose_classification_filter(pose_classification)
+            # Smooth classification using EMA.
+            pose_classification_filtered = pose_classification_filter(pose_classification)
 
-      # Count repetitions.
-      repetitions_count = repetition_counter(pose_classification_filtered)
-    else:
-      # No pose => no classification on current frame.
-      pose_classification = None
+            # Count repetitions.
+            repetitions_count = repetition_counter(pose_classification_filtered)
+        else:
+            # No pose => no classification on current frame.
+            pose_classification = None
 
-      # Still add empty classification to the filter to maintaing correct
-      # smoothing for future frames.
-      pose_classification_filtered = pose_classification_filter(dict())
-      pose_classification_filtered = None
+            # Still add empty classification to the filter to maintaing correct
+            # smoothing for future frames.
+            pose_classification_filtered = pose_classification_filter(dict())
+            pose_classification_filtered = None
 
-      # Don't update the counter presuming that person is 'frozen'. Just
-      # take the latest repetitions count.
-      repetitions_count = repetition_counter.n_repeats
+            # Don't update the counter presuming that person is 'frozen'. Just
+            # take the latest repetitions count.
+            repetitions_count = repetition_counter.n_repeats
 
-    # Draw classification plot and repetition counter.
-    output_frame = pose_classification_visualizer(
-        frame=output_frame,
-        pose_classification=pose_classification,
-        pose_classification_filtered=pose_classification_filtered,
-        repetitions_count=repetitions_count)
+        # Draw classification plot and repetition counter.
+        output_frame = pose_classification_visualizer(
+                frame=output_frame,
+                pose_classification=pose_classification,
+                pose_classification_filtered=pose_classification_filtered,
+                repetitions_count=repetitions_count)
 
-    # Save the output frame.
-    out_video.write(cv2.cvtColor(np.array(output_frame), cv2.COLOR_RGB2BGR))
+        # Save the output frame.
+        out_video.write(cv2.cvtColor(np.array(output_frame), cv2.COLOR_RGB2BGR))
 
-    cv2.imshow('MediaPipe', cv2.cvtColor(np.array(output_frame), cv2.COLOR_RGB2BGR))
+        cv2.imshow('MediaPipe', cv2.cvtColor(np.array(output_frame), cv2.COLOR_RGB2BGR))
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-      break
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
-    frame_idx += 1
-    pbar.update()
+        frame_idx += 1
+        pbar.update()
 
 # Close output video.
 out_video.release()
@@ -155,5 +154,3 @@ pose_tracker.close()
 
 video_cap.release()
 cv2.destroyAllWindows()
-
-
