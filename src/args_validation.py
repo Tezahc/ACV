e90e7b59ea1dde35761b7578759ad472ce347d75
Léo CHAUSSY <chaussyleo@gmail.com>
2025-07-09 10:28:11 +0200

    add arg plot

--- src/args_validation.py
+++ src/args_validation.py
@@ -4,5 +4,6 @@ def args_validation(parser):
     
     parser.add_argument('-i', '--input_video_file', type=str, default=0)
     parser.add_argument('-o', '--output_video_file', type=str, required=False)
+    parser.add_argument("-p", "--show_plots", action='store_true', help="Enable matplotlib plots")
 
     return parser.parse_args()
